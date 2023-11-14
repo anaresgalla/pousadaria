70aa1406b8f4bdf94282d443b857135f4cb38c40
anaresgalla <anaresgalla@gmail.com>
2023-11-14 19:12:37 -0300

    Booking dates overlap

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,11 +1,36 @@
 class Booking < ApplicationRecord
   validates :check_in, :check_out, :guests, :total_price, presence: true
   validate :verify_number_of_guests
+  validate :overlapping
   belongs_to :room
+
+  private
 
   def verify_number_of_guests    
     if guests.present? && room.present? && guests > room.max_guests
       errors.add(:base, 'O número de hóspedes excede a capacidade máxima do quarto.')
     end 
+  end
+
+  def overlapping
+    result = true
+    booked = {}
+
+    Booking.pluck(:check_in, :check_out).each do |date_range|
+        (date_range.first..date_range.second).each do |date| 
+            booked[date] = true
+        end
+    end
+
+    if booked.has_key? self.check_in
+        errors.add(:base, 'O quarto não está disponível na data de entrada.')
+        result = false
+    end
+
+    if booked.has_key? self.check_out
+        errors.add(:base, 'O quarto não está disponível na data de saída.')
+        result = false
+    end
+    result
   end
 end
--- spec/system/user_checks_vacant_rooms_spec.rb
+++ spec/system/user_checks_vacant_rooms_spec.rb
@@ -93,42 +93,45 @@ describe 'User checks vacant rooms of a lodge' do
   #   click_on 'Pousada do Mar'
   #   click_on 'Pérola Negra'
   #   click_on 'Reservar'
-  #   fill_in 'Check In', with: 'DATA'
-  #   fill_in 'Check Out', with: 'DATA'
-  #   fill_in 'Número de Hóspedes', with: 'xxxxx'
+  #   fill_in 'Check In', with: 1.day.from_now
+  #   fill_in 'Check Out', with: 10.days.from_now
+  #   fill_in 'Número de Hóspedes', with: 2
+  #   fill_in 'Valor Total', with: 1350
+  #   click_on 'Consultar Disponibilidade'
 
   #   #Assert
   #   expect(page).to have_content 'O quarto está disponível.'
-  #   expect(page).to have_content 'Valor Total das Diárias:'
+  #   expect(page).to have_content 'Valor Total:'
   # end
 
-  # it 'and the room is not vacant' do
-  #   #Arrange
-  #   owner = Owner.create!(name: 'Carla Mendonça', email: 'carsampa@gmail.com', password: '123456')
-  #   lodge = Lodge.create(name: 'Pousada do Mar', address: 'Avenida Beira Mar, 1500', neighborhood: 'Coqueiros', city: 'Marataízes',
-  #                        state: 'ES', country: 'Brasil', zip_code: '12345-985', description: 'Pousada em frente à praia', bedrooms: 5, 
-  #                        max_guests: 12, pets: 'Sim', disabled_facilities: 'Menu em Braile', check_in: '15:00', check_out: '12:00', 
-  #                        status: 'Ativa', email: 'pousadadomar@gmail.com', phone_number: '28985647114', 
-  #                        corporate_name: 'Almeida e Filhos LTDA', cnpj: '08945909000124', payment_method: "Cartão de crédito, Pix", 
-  #                        policies: 'Proibido fumar no local. Silêncio a partir das 22h.', owner: owner)
-  #   room = Room.create!(name: 'Pérola Negra', description: 'Quarto de frente para o mar', area: '15 m²', max_guests: 2,
-  #                       standard_overnight: '150,00 BRL', bathroom: 'Sim', balcony: 'Sim', ac: 'Sim', tv: 'Sim', 
-  #                       closet: 'Sim', disabled_facilities: 'Sim', safe: 'Não', vacant: 'Disponível', lodge: lodge)
+  it 'and the room is not vacant' do
+    #Arrange
+    owner = Owner.create!(name: 'Carla Mendonça', email: 'carsampa@gmail.com', password: '123456')
+    lodge = Lodge.create(name: 'Pousada do Mar', address: 'Avenida Beira Mar, 1500', neighborhood: 'Coqueiros', city: 'Marataízes',
+                         state: 'ES', country: 'Brasil', zip_code: '12345-985', description: 'Pousada em frente à praia', bedrooms: 5, 
+                         max_guests: 12, pets: 'Sim', disabled_facilities: 'Menu em Braile', check_in: '15:00', check_out: '12:00', 
+                         status: 'Ativa', email: 'pousadadomar@gmail.com', phone_number: '28985647114', 
+                         corporate_name: 'Almeida e Filhos LTDA', cnpj: '08945909000124', payment_method: "Cartão de crédito, Pix", 
+                         policies: 'Proibido fumar no local. Silêncio a partir das 22h.', owner: owner)
+    room = Room.create!(name: 'Pérola Negra', description: 'Quarto de frente para o mar', area: '15 m²', max_guests: 2,
+                        standard_overnight: '150,00 BRL', bathroom: 'Sim', balcony: 'Sim', ac: 'Sim', tv: 'Sim', 
+                        closet: 'Sim', disabled_facilities: 'Sim', safe: 'Não', vacant: 'Disponível', lodge: lodge)
+    booking = Booking.create!(check_in: 1.day.from_now, check_out: 5.days.from_now, guests: 2, total_price: 100, room: room)
 
-  #   #Act
-  #   visit root_path
-  #   click_on 'Pousada do Mar'
-  #   click_on 'Pérola Negra'
-  #   click_on 'Reservar'
-  #   fill_in 'Check In', with: 'DATA'
-  #   fill_in 'Check Out', with: 'DATA'
-  #   fill_in 'Número de Hóspedes', with: 'xxxxx'
+    #Act
+    visit root_path
+    click_on 'Pousada do Mar'
+    click_on 'Pérola Negra'
+    click_on 'Reservar'
+    fill_in 'Data de Entrada', with: 2.days.from_now
+    fill_in 'Data de Saída', with: 6.days.from_now
+    fill_in 'Número de Hóspedes', with: 1
+    fill_in 'Valor Total', with: 150
+    click_on 'Consultar Disponibilidade'
 
-  #   #Assert
-  #   expect(page).to have_content 'Desculpe, mas o quarto não está disponível.'
-  #   expect(page).not_to have_content 'O quarto está disponível.'
-  #   expect(current_page).to eq RESERVATIONPATH
-  # end
+    #Assert
+    expect(page).to have_content 'O quarto não está disponível na data de entrada.'
+  end
 
   it 'and the number of guests exceeds the maximum' do
     #Arrange
